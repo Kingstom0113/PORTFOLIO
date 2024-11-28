b7de45fb05d46d1c7ebc0573ed17323d948a473b
Kingstom0113 <manuelsr0113@gmail.com>
2024-11-28 03:58:07 +0100

    Initialize Astro project with TypeScript, Tailwind CSS, and basic structure

---
+++ deeply-disk/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Asegúrate de incluir las extensiones que usas
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
