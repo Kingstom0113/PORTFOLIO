e72ff0bd01142fbd145b43e1f95f47fe830fa2d2
Eskell <rubenvazquezsfc@gmail.com>
2025-03-12 19:55:14 +0100

    intento desplegarlo en gh pages

--- deeply-disk/astro.config.mjs
+++ deeply-disk/astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import  tailwind  from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+  site: 'https://kingstom0113.github.io',
+  base: '/PORTFOLIO',
+  integrations: [tailwind()],
+  output: 'static',
+  trailingSlash: 'always'
 });
