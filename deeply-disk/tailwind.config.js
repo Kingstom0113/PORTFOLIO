2eb361db9a6aeedac2db9b94c0963c8bc9eb218c
Eskell <rubenvazquezsfc@gmail.com>
2025-03-12 18:13:02 +0100

    mismo

--- deeply-disk/tailwind.config.js
+++ deeply-disk/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Asegúrate de incluir las extensiones que usas
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
     './public/**/*.html',
   ],
   theme: {
