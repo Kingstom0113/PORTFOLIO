ba9234c2906388408374a14e09c9f3123d735b85
Eskell <rubenvazquezsfc@gmail.com>
2025-03-12 18:24:44 +0100

    Initial commit - Portfolio setup

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://kingstom0113.github.io',
+  base: '/PORTFOLIO',
+  integrations: [tailwind()],
+  output: 'static',
+});
